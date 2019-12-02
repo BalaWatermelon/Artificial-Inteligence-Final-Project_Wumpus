4e049dbcce433312d80654d92f7e653390f92d47
Jackson Lin <jackson1994y+git@gmail.com>
2019-12-02 00:35:52 -0500

    Format doc

--- agent.py
+++ agent.py
@@ -1,31 +1,27 @@
-#%%
-def ifAdjSafe(safe,back,statusM):
-    M=statusM
-    if M=='*':
-        adj=m.adjacent()
+# %%
+def ifAdjSafe(safe, back, statusM):
+    M = statusM
+    if M == '*':
+        adj = m.adjacent()
         for i in adj:
             if i not in back:
                 safe.append(i)
 
-    
-    
-#%%
-import map
-m=map.Map()
-safe=[]
-back=[]
 
-ifAdjSafe(safe,back,m.status())
+# %%
+import map
+m = map.Map()
+safe = []
+back = []
+
+ifAdjSafe(safe, back, m.status())
 
 while safe:
-    nextstep=safe.pop()
+    nextstep = safe.pop()
     back.append(m.whereAmI())
     m.moveTo(nextstep)
-    ifAdjSafe(safe,back,m.status())
+    ifAdjSafe(safe, back, m.status())
     print(m.whereAmI())
-    
-
-
 
 
 # %%
